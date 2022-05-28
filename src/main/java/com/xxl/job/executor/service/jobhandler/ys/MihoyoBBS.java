0bc0c4816eafc948689b4a2b81f6cd408f830692
liliya <1752181917@qq.com>
2022-05-28 16:31:39 +0800

    getset

--- src/main/java/com/xxl/job/executor/service/jobhandler/ys/MihoyoBBS.java
+++ src/main/java/com/xxl/job/executor/service/jobhandler/ys/MihoyoBBS.java
@@ -35,5 +35,31 @@ public class MihoyoBBS {
 
         cookie = GlobalTool.loadCookie();
         articleList = GlobalTool.loadArticleList(headers);
+
+    }
+
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+    public List<String> getArticleList() {
+        return articleList;
+    }
+
+    public void setArticleList(List<String> articleList) {
+        this.articleList = articleList;
     }
 }
